ad4dc749b31eecff69c9708ec7618a813c94cc5e
Magnus Rud <mag.aars@gmail.com>
2021-11-08 16:59:14 +0100

    update javadocs

--- static/java-docs/search/all_d.js
+++ static/java-docs/search/all_d.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['transitionexecutionstatus',['TransitionExecutionStatus',['../enumai_1_1lucidtech_1_1las_1_1sdk_1_1_transition_execution_status.html',1,'ai::lucidtech::las::sdk']]],
-  ['transitionparameters',['TransitionParameters',['../classai_1_1lucidtech_1_1las_1_1sdk_1_1_transition_parameters.html',1,'ai::lucidtech::las::sdk']]],
-  ['transitiontype',['TransitionType',['../enumai_1_1lucidtech_1_1las_1_1sdk_1_1_transition_type.html',1,'ai::lucidtech::las::sdk']]]
+  ['sendheartbeat',['sendHeartbeat',['../classai_1_1lucidtech_1_1las_1_1sdk_1_1_client.html#ae78d4889966bcacf3a8fc8c41d31de4d',1,'ai::lucidtech::las::sdk::Client']]]
 ];
