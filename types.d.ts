0badd7abbeb6212accc14876f945c9f9583b358b
Roger Andersen <roger@lucidtech.ai>
2021-11-23 15:52:38 +0100

    fix typescript declaration

---
+++ types.d.ts
@@ -0,0 +1,7 @@
+declare module '*.svg' {
+  import type { ComponentType, SVGProps } from 'react';
+
+  const ReactComponent: ComponentType<SVGProps<SVGSVGElement>>;
+
+  export default ReactComponent;
+}
