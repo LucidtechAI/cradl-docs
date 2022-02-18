4ba0c910ebfb269753696637fe752a03c21b2afd
Ståle Zerener Haugnæss <staale@lucidtech.ai>
2022-02-18 16:26:21 +0100

    Added pretty icons, etc.

--- src/components/HeaderText.tsx
+++ src/components/HeaderText.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import styles from './HeaderText.module.css';
 
 
-export default ({ title, text, width = '300px' }) => (
-    <div>
-        <h3>{title}</h3>
-        <p>{text}</p>
-    </div>
+export default ({ title, text, icon, width = '300px' }) => (
+  <div className={styles.container}>
+      <div className={styles.header}>
+          { icon && <img src={icon} /> }
+          <h3>{title}</h3></div>
+      <p>{text}</p>
+  </div>
 );
