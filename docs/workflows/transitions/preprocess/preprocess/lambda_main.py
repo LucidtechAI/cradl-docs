31cff68b569b0d35f1b7794f13c9cb7e2b22973e
Magnus Rud <mag.aars@gmail.com>
2024-04-15 16:05:38 +0200

    environment variables comes as a list

--- docs/workflows/transitions/preprocess/preprocess/lambda_main.py
+++ docs/workflows/transitions/preprocess/preprocess/lambda_main.py
@@ -1,6 +1,6 @@
 from .make_predictions import make_predictions
 
 def main(lambda_event, _):
-    for key, value in lambda_event.items():
-        os.environ[key] = value
+    for environment_variable in lambda_event:
+        os.environ[environment_variable['name']] = environment_variable['value']
     make_predictions()
