f450fb74e664fbe67902ec9cecb607e321636d6a
Ståle Z.H <staalezh@users.noreply.github.com>
2024-04-19 13:58:17 +0200

    Updated feedback in postprocessing image (#197)
    * Include page information, etc. in new feedback
    
    * Improved error handling and test coverage
    
    * Fixed bug.
    
    * Simplify logic
    
    Co-authored-by: August André Kvernmo <august@lucidtech.ai>
    
    * Better error handling
    
    Co-authored-by: August André Kvernmo <august@lucidtech.ai>
    
    * Handle errors from requests.
    
    * Improved error handling + simplified logic.
    
    * Updated tests.
    
    * Fixed linting erorrs.
    
    * Include original predictions in the output, on v2 format.
    
    * Fixed linting + imports
    
    * Fixed linting errors in tests.
    
    ---------
    
    Co-authored-by: Ståle Zerener Haugnæss <staale@lucidtech.ai>
    Co-authored-by: August André Kvernmo <august@lucidtech.ai>

--- docs/workflows/transitions/postprocess/postprocess/feedback_and_export.py
+++ docs/workflows/transitions/postprocess/postprocess/feedback_and_export.py
@@ -3,51 +3,124 @@ import os
 import las
 import requests
 
+from .utils import parse_webhook_endpoints, convert_predictions_to_v2
 
-def post_feedback(las_client: las.Client, document_id: str, dataset_id: str, verified: dict):
-    logging.info(f'Posting feedback to dataset {dataset_id}: {verified}...')
 
-    try:
-        document = las_client.get_document(document_id=document_id)
-        old_ground_truth = {g['label']: g['value'] for g in document.get('groundTruth', [])}
+def post_feedback_v1(las_client: las.Client, document_id: str, dataset_id: str, verified: dict):
+    document = las_client.get_document(document_id=document_id)
+    old_ground_truth = {g['label']: g['value'] for g in document.get('groundTruth', [])}
 
-        new_ground_truth = {**old_ground_truth, **verified}
+    new_ground_truth = {**old_ground_truth, **verified}
 
-        ground_truth = []
-        for label, value in new_ground_truth.items():
-            if isinstance(value, list):
-                if not value:
-                    continue  # Do not write completely empty lines to GT
-                if not isinstance(value[0], list):
-                    value = [[{'label': k, 'value': v} for k, v in line_pred.items()] for line_pred in value]
-            ground_truth.append({
+    ground_truth = []
+    for label, value in new_ground_truth.items():
+        if isinstance(value, list):
+            if not value:
+                continue  # Do not write completely empty lines to GT
+            if not isinstance(value[0], list):
+                value = [[{'label': k, 'value': v} for k, v in line_pred.items()] for line_pred in value]
+        ground_truth.append({
+            'label': label,
+            'value': value,
+        })
+
+    las_client.update_document(
+        document_id=document_id,
+        ground_truth=ground_truth,
+        dataset_id=dataset_id
+    )
+
+
+def post_feedback_v2(las_client: las.Client, document_id: str, dataset_id: str, feedback: dict):
+    document = las_client.get_document(document_id=document_id)
+    old_ground_truth = {g['label']: {**g, 'isOldGt': True} for g in document.get('groundTruth', [])}
+
+    def should_post_feedback(v):
+        return not v.get('automated', True) or v.get('isEdited', True) or v.get('isOldGt', False)
+
+    ground_truth = []
+
+    for label, value in {**old_ground_truth, **feedback}.items():
+        is_line_item = isinstance(value, list)
+        if is_line_item:
+            if not value:
+                # Ignore blank lines
+                continue
+
+            lines = []
+            for line in value:
+                line_gt = []
+                for line_label, line_value in line.items():
+                    if should_post_feedback(line_value):
+                        line_gt += [{
+                            'label': line_label,
+                            'value': line_value['value'],
+                            'pages': line_value.get('pages')
+                        }]
+
+                lines += [line_gt]
+
+            ground_truth += [{
                 'label': label,
-                'value': value,
-            })
+                'value': lines,
+            }]
+        else:
+            # This is a non-line item field
+            if should_post_feedback(value):
+                ground_truth += [{
+                    'label': label,
+                    'value': value['value'],
+                    'pages': value.get('pages')
+                }]
 
-        las_client.update_document(
-            document_id=document_id,
-            ground_truth=ground_truth,
-            dataset_id=dataset_id
-        )
-    except Exception as e:
-        logging.exception(e)
-        raise
+    las_client.update_document(
+        document_id=document_id,
+        ground_truth=ground_truth,
+        dataset_id=dataset_id
+    )
 
 
 @las.transition_handler
 def feedback_and_export(las_client, event):
     document_id = event['documentId']
-    verified = event['verified']
     dataset_id = os.environ.get('DATASET_ID')
-    skipped_validation = not event.get('needsValidation', True)
+    validated = event.get('needsValidation', True) is True
+    feedback_v1 = event.get('verified')
+    feedback_v2 = event.get('validatedPredictions')
 
-    post_feedback(las_client, document_id, dataset_id, verified if not skipped_validation else {})
+    try:
+        logging.info(f'Posting feedback to dataset {dataset_id} ...')
 
-    response = {'documentId': document_id, 'datasetId': dataset_id, 'values': verified}
+        if feedback_v2:
+            post_feedback_v2(las_client, document_id, dataset_id, feedback_v2 if validated else {})
+        elif feedback_v1:
+            post_feedback_v1(las_client, document_id, dataset_id, feedback_v1 if validated else {})
+    except Exception as e:
+        logging.exception(e)
 
-    if webhook_uri := os.environ.get('WEBHOOK_URI'):
-        logging.info(f'Posting result to {webhook_uri}...')
-        requests.post(webhook_uri, json=response)
+    response = {
+        'documentId': document_id,
+        'datasetId': dataset_id,
+        'values': feedback_v1,
+        'validatedPredictions': feedback_v2,
+        'predictions': convert_predictions_to_v2(event.get('predictions'))
+    }
+
+    webhook_endpoints = []
+    if webhooks := os.environ.get('WEBHOOK_ENDPOINTS'):
+        webhook_endpoints = parse_webhook_endpoints(webhooks)
+    elif uri := os.environ.get('WEBHOOK_URI'):
+        webhook_endpoints = [{'uri': uri}]
+
+    request_exception = None
+    for endpoint in webhook_endpoints:
+        logging.info(f'Posting result to {endpoint}...')
+        try:
+            requests.post(endpoint['uri'], json=response)
+        except (requests.exceptions.RequestException, KeyError) as re:
+            request_exception = re
+
+    if request_exception:
+        raise request_exception
 
     return response
--- docs/workflows/transitions/postprocess/tests/test_handler.py
+++ docs/workflows/transitions/postprocess/tests/test_handler.py
@@ -4,7 +4,7 @@ import base64
 import runpy
 import requests_mock
 
-from unittest.mock import patch
+from unittest.mock import patch, ANY
 
 
 @pytest.fixture
@@ -19,13 +19,25 @@ def env():
     yield {
         'TRANSITION_ID': 'xyz',
         'EXECUTION_ID': 'xyz',
+        'DATASET_ID': 'las:dataset:xyz',
         'MODEL_ID': 'las:model:xyz',
     }
 
 
 @pytest.fixture
-def env_with_webhook(env):
-    yield {**env, 'WEBHOOK_URI': 'https://foo.bar/'}
+def env_with_webhook_uri(env):
+    yield {
+        **env,
+        'WEBHOOK_URI': 'https://foo.bar/',
+    }
+
+
+@pytest.fixture
+def env_with_webhook_endpoints(env):
+    yield {
+        **env,
+        'WEBHOOK_ENDPOINTS': '[{"uri": "https://foo.bar/"}, {"uri": "https://baz.bar/"}]'
+    }
 
 
 @patch('las.Client.get_transition_execution')
@@ -42,15 +54,15 @@ def test_handler(get_document, get_asset, update_document, update_transition_exc
         }
     }
 
-    with patch.dict('postprocess.postprocess.feedback_and_export.os.environ', env):
-        runpy.run_module('postprocess.postprocess', run_name='__main__')
+    with patch.dict('postprocess.feedback_and_export.os.environ', env):
+        runpy.run_module('postprocess', run_name='__main__')
 
 
 @patch('las.Client.get_transition_execution')
 @patch('las.Client.update_transition_execution')
 @patch('las.Client.update_document')
 @patch('las.Client.get_document')
-def test_webhook(get_document, update_document, update_transition_excs, get_transition_excs, env_with_webhook):
+def test_webhook(get_document, update_document, update_transition_excs, get_transition_excs, env_with_webhook_uri):
     get_transition_excs.return_value = {
         'input': {
             'documentId': 'las:document:xyz',
@@ -59,15 +71,48 @@ def test_webhook(get_document, update_document, update_transition_excs, get_tran
         }
     }
 
-    with patch.dict('postprocess.postprocess.feedback_and_export.os.environ', env_with_webhook):
+    with patch.dict('postprocess.feedback_and_export.os.environ', env_with_webhook_uri):
         with requests_mock.Mocker() as m:
-            m.post(env_with_webhook['WEBHOOK_URI'])
-            runpy.run_module('postprocess.postprocess', run_name='__main__')
+            m.post(env_with_webhook_uri['WEBHOOK_URI'])
+            runpy.run_module('postprocess', run_name='__main__')
             assert m.call_count == 1
 
             history = m.request_history[0]
             assert history.method == 'POST'
-            assert history.url == env_with_webhook['WEBHOOK_URI']
+            assert history.url == env_with_webhook_uri['WEBHOOK_URI']
+
+            for key in ['documentId', 'datasetId', 'values']:
+                assert key in history.json()
+
+
+@patch('las.Client.get_transition_execution')
+@patch('las.Client.update_transition_execution')
+@patch('las.Client.update_document')
+@patch('las.Client.get_document')
+def test_multiple_webhook_endpoints(
+    get_document, update_document, update_transition_excs,
+    get_transition_excs, env_with_webhook_endpoints
+):
+    get_transition_excs.return_value = {
+        'input': {
+            'documentId': 'las:document:xyz',
+            'needsValidation': True,
+            'verified': {}
+        }
+    }
+
+    with patch.dict('postprocess.feedback_and_export.os.environ', env_with_webhook_endpoints):
+        with requests_mock.Mocker() as m:
+            endpoints = json.loads(env_with_webhook_endpoints['WEBHOOK_ENDPOINTS'])
+            for endpoint in endpoints:
+                m.post(endpoint['uri'])
+
+            runpy.run_module('postprocess', run_name='__main__')
+            assert m.call_count == len(endpoints)
+
+            history = m.request_history[0]
+            assert history.method == 'POST'
+            assert history.url == endpoints[0]['uri']
 
             for key in ['documentId', 'datasetId', 'values']:
                 assert key in history.json()
@@ -89,8 +134,8 @@ def test_update_ground_truth(get_document, update_document, update_transition_ex
 
     get_document.return_value = {'groundTruth': [{'label': 'baz', 'value': 'foobar'}]}
 
-    with patch.dict('postprocess.postprocess.feedback_and_export.os.environ', env):
-        runpy.run_module('postprocess.postprocess', run_name='__main__')
+    with patch.dict('postprocess.feedback_and_export.os.environ', env):
+        runpy.run_module('postprocess', run_name='__main__')
 
     update_document.assert_called_with(
         document_id='las:document:xyz',
@@ -101,7 +146,7 @@ def test_update_ground_truth(get_document, update_document, update_transition_ex
             'label': 'foo',
             'value': 'bar'
         }],
-        dataset_id=None
+        dataset_id='las:dataset:xyz'
     )
 
 
@@ -115,6 +160,7 @@ def test_update_ground_truth_with_lines(
     get_transition_excs.return_value = {
         'input': {
             'documentId': 'las:document:xyz',
+            'needsValidation': True,
             'verified': {
                 'foo': 'bar',
                 'line_items': [
@@ -143,8 +189,8 @@ def test_update_ground_truth_with_lines(
             [{'label': 'prod', 'value': 'def'}, {'label': 'unit_price', 'value': '20.00'}]]}
     ]}
 
-    with patch.dict('postprocess.postprocess.feedback_and_export.os.environ', env):
-        runpy.run_module('postprocess.postprocess', run_name='__main__')
+    with patch.dict('postprocess.feedback_and_export.os.environ', env):
+        runpy.run_module('postprocess', run_name='__main__')
 
     update_document.assert_called_with(
         document_id='las:document:xyz',
@@ -186,7 +232,7 @@ def test_update_ground_truth_with_lines(
             'label': 'purchase_date',
             'value': '2023-09-21'
         }],
-        dataset_id=None
+        dataset_id='las:dataset:xyz'
     )
 
 
@@ -200,6 +246,7 @@ def test_update_ground_truth_with_same_lines(
     get_transition_excs.return_value = {
         'input': {
             'documentId': 'las:document:xyz',
+            'needsValidation': True,
             'verified': {
                 'foo': 'bar',
                 'line_items': [
@@ -228,8 +275,8 @@ def test_update_ground_truth_with_same_lines(
             [{'label': 'description', 'value': 'def'}, {'label': 'total_price', 'value': '20.00'}]]}
     ]}
 
-    with patch.dict('postprocess.postprocess.feedback_and_export.os.environ', env):
-        runpy.run_module('postprocess.postprocess', run_name='__main__')
+    with patch.dict('postprocess.feedback_and_export.os.environ', env):
+        runpy.run_module('postprocess', run_name='__main__')
 
     update_document.assert_called_with(
         document_id='las:document:xyz',
@@ -259,7 +306,7 @@ def test_update_ground_truth_with_same_lines(
             'label': 'purchase_date',
             'value': '2023-09-21'
         }],
-        dataset_id=None
+        dataset_id='las:dataset:xyz'
     )
 
 
@@ -273,6 +320,7 @@ def test_update_ground_truth_with_empty_lines(
     get_transition_excs.return_value = {
         'input': {
             'documentId': 'las:document:xyz',
+            'needsValidation': True,
             'verified': {
                 'foo': 'bar',
                 'line_items': [],
@@ -286,8 +334,8 @@ def test_update_ground_truth_with_empty_lines(
         {'label': 'baz', 'value': 'foobar'},
     ]}
 
-    with patch.dict('postprocess.postprocess.feedback_and_export.os.environ', env):
-        runpy.run_module('postprocess.postprocess', run_name='__main__')
+    with patch.dict('postprocess.feedback_and_export.os.environ', env):
+        runpy.run_module('postprocess', run_name='__main__')
 
     update_document.assert_called_with(
         document_id='las:document:xyz',
@@ -304,5 +352,40 @@ def test_update_ground_truth_with_empty_lines(
             'label': 'string_value',
             'value': '',
         }],
-        dataset_id=None
+        dataset_id='las:dataset:xyz'
+    )
+
+
+@patch('las.Client.get_transition_execution')
+@patch('las.Client.update_transition_execution')
+@patch('las.Client.update_document')
+@patch('las.Client.get_document')
+def test_post_feedback_v2(get_document, update_document, update_transition_excs, get_transition_excs, env):
+    validated_predictions = {
+        'totalAmount': {"value": "100.00", "pages": [0, 1], "confidence": 1.0},
+        'line_items': [{
+            "unitPrice": {"value": "50.00", "pages": [0], "confidence": 0.8},
+            "totalPrice": {"value": "100.00", "pages": [0], "confidence": 0.9}
+        }, {
+            "unitPrice": {"value": "50.00", "pages": [0], "confidence": 0.8},
+            "totalPrice": {"value": "100.00", "pages": [0], "confidence": 0.9}
+        }]
+    }
+
+    get_transition_excs.return_value = {
+        'input': {
+            'documentId': 'las:document:xyz',
+            'validatedPredictions': validated_predictions
+        }
+    }
+
+    get_document.return_value = {'groundTruth': [{'label': 'baz', 'value': 'foobar'}]}
+
+    with patch.dict('postprocess.feedback_and_export.os.environ', env):
+        runpy.run_module('postprocess', run_name='__main__')
+
+    update_document.assert_called_with(
+        document_id='las:document:xyz',
+        ground_truth=ANY,
+        dataset_id='las:dataset:xyz'
     )
