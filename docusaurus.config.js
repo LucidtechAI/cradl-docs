45638ffa3d2bbd3cd21351f617742782207714dd
Kavian Braanaas <kavianbraanaas@Kavians-MacBook-Air.local>
2024-06-13 12:38:46 +0200

    fixed social card filetype

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -82,7 +82,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/cradl-social-card.jpg',
+      image: 'img/cradl-social-card.png',
 
       navbar: {
         logo: {
