02e1c6d46a0c9995864920e9f86cd2680b185b30
August André Kvernmo <august@lucidtech.ai>
2024-10-21 09:30:43 +0200

    Parallel requests upto MAX_WORKERS if we can get number of pages from document (#221)
    * Parallel requests upto MAX_WORKERS if we can get number of pages from document
    
    * lint
    
    * Updates
    
    * Updates
    
    * Update
    
    * Bugfix
    
    * Update
    
    * update
    
    * Update

--- docs/workflows/transitions/preprocess/preprocess/make_predictions.py
+++ docs/workflows/transitions/preprocess/preprocess/make_predictions.py
@@ -1,10 +1,15 @@
+import base64
+import copy
+import io
+import json
 import logging
 import os
-import las
-import json
-import base64
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from functools import partial
 
+import las
 from backoff import constant, on_exception
+from pypdf import PdfReader
 
 from .utils import (
     above_threshold_or_optional,
@@ -46,11 +51,18 @@ def _create_prediction(las_client, document_id, model_id, preprocess_config):
     )
 
 
+def get_num_pages(las_client, document_id):
+    document = las_client.get_document(document_id)
+    pdf = PdfReader(io.BytesIO(base64.b64decode(document['content'])))
+    return min(len(pdf.pages), 100)
+
+
 @las.transition_handler
 def make_predictions(las_client, event):
     document_id = event['documentId']
     model_id = os.environ['MODEL_ID']
     form_config_id = os.environ['FORM_CONFIG_ASSET_ID']
+    max_workers = int(os.environ.get('MAX_WORKERS', '2'))
 
     logging.info('Processing event:')
     logging.info(json.dumps(event, indent=2))
@@ -61,6 +73,7 @@ def make_predictions(las_client, event):
     model = las_client.get_model(model_id)
     model_metadata = model.get('metadata', {})
     preprocess_config = model.get('preprocessConfig', {})
+    preprocess_config['maxPages'] = 1
     model_field_config = model.get('fieldConfig', {})
     logging.info(f'model metadata: {model_metadata}')
 
@@ -78,31 +91,67 @@ def make_predictions(las_client, event):
     no_empty_prediction_fields = set()
 
     if not (predictions := event.get('predictions')):
-        start_page = 0
-        predictions = []
-        while start_page is not None and start_page < model_metadata.get('maxPredictionPages', 100):
-            try:
-                preprocess_config['startPage'] = start_page
-                preprocess_config['maxPages'] = 1
-                current_prediction = _create_prediction(
-                    las_client=las_client,
-                    document_id=document_id,
-                    model_id=model_id,
-                    preprocess_config=preprocess_config,
-                )
-                start_page = current_prediction.get('nextPage')
-                current_prediction = current_prediction['predictions']
+        prediction_fn = partial(
+            _create_prediction,
+            las_client=las_client,
+            document_id=document_id,
+            model_id=model_id,
+        )
+        try:
+            num_pages = get_num_pages(las_client, document_id)
+        except Exception as e:
+            logging.exception(e)
+            num_pages = None
 
-                fields_with_empty_prediction = set(
-                    prediction['label'] for prediction in current_prediction if prediction['value'] is None
-                )
-                no_empty_prediction_fields = no_empty_prediction_fields.union(labels - fields_with_empty_prediction)
+        if num_pages:
+            predictions = []
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                futures = {}
+                for start_page in range(num_pages):
+                    config = copy.deepcopy(preprocess_config)
+                    config['startPage'] = start_page
+                    futures[executor.submit(prediction_fn, preprocess_config=config)] = start_page
+                results = {}
+                for future in as_completed(futures):
+                    try:
+                        current_prediction = future.result()['predictions']
+                        start_page = futures[future]
+                        logging.info(f'Finished predictions for start_page: {start_page}')
+                        fields_with_empty_prediction = set(
+                            prediction['label'] for prediction in current_prediction if prediction['value'] is None
+                        )
+                        no_empty_prediction_fields = no_empty_prediction_fields.union(
+                            labels - fields_with_empty_prediction
+                        )
+                        results[start_page] = current_prediction
+                    except Exception as e:
+                        logging.exception(e)
+                for i, prediction in sorted(results.items()):
+                    logging.info(f'Extending predictions list with predictions from page {i}')
+                    predictions.extend(prediction)
 
-                predictions.extend(current_prediction)
-                logging.info(f'new start_page {start_page}')
-            except Exception as e:
-                logging.exception(e)
-                break
+        else:
+            start_page = 0
+            predictions = []
+            while start_page is not None and start_page < model_metadata.get('maxPredictionPages', 100):
+                try:
+                    preprocess_config['startPage'] = start_page
+                    current_prediction = prediction_fn(preprocess_config=preprocess_config)
+                    start_page = current_prediction.get('nextPage')
+                    current_prediction = current_prediction['predictions']
+
+                    fields_with_empty_prediction = set(
+                        prediction['label'] for prediction in current_prediction if prediction['value'] is None
+                    )
+                    no_empty_prediction_fields = no_empty_prediction_fields.union(
+                        labels - fields_with_empty_prediction
+                    )
+
+                    predictions.extend(current_prediction)
+                    logging.info(f'new start_page {start_page}')
+                except Exception as e:
+                    logging.exception(e)
+                    break
 
     logging.info(f'Created predictions {predictions}')
 
