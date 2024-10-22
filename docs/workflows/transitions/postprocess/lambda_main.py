2b1cd1f7f9fccbd9df162075670e536d22debc7b
Magnus Rud <mag.aars@gmail.com>
2024-10-22 13:45:56 +0200

    wipe old environment

--- docs/workflows/transitions/postprocess/lambda_main.py
+++ docs/workflows/transitions/postprocess/lambda_main.py
@@ -2,8 +2,18 @@ import os
 
 from postprocess.feedback_and_export import feedback_and_export
 
+CUSTOMER_ENVIRONMENT_VARIABLES = [
+    'DATASET_ID',
+    'MODEL_ID',
+    'WEBHOOK_ENDPOINTS',
+    'WEBHOOK_URI',
+]
+
 
 def main(lambda_event, _):
+    for key in CUSTOMER_ENVIRONMENT_VARIABLES:
+        os.environ.pop(key, None)
+
     for environment_variable in lambda_event:
         os.environ[environment_variable['name']] = environment_variable['value']
     feedback_and_export()
