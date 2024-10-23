bdbc8489c5335dafc8ba1b2633bc85e3ee575f63
Magnus Rud <mag.aars@gmail.com>
2024-10-23 11:11:06 +0200

    Wipe everything but common environment

--- docs/workflows/transitions/postprocess/lambda_main.py
+++ docs/workflows/transitions/postprocess/lambda_main.py
@@ -2,18 +2,54 @@ import os
 
 from postprocess.feedback_and_export import feedback_and_export
 
-CUSTOMER_ENVIRONMENT_VARIABLES = [
-    'DATASET_ID',
-    'MODEL_ID',
-    'WEBHOOK_ENDPOINTS',
-    'WEBHOOK_URI',
+
+COMMON_ENV = [
+    'AWS_ACCESS_KEY_ID',
+    'AWS_DEFAULT_REGION',
+    'AWS_EXECUTION_ENV',
+    'AWS_LAMBDA_FUNCTION_MEMORY_SIZE',
+    'AWS_LAMBDA_FUNCTION_NAME',
+    'AWS_LAMBDA_FUNCTION_VERSION',
+    'AWS_LAMBDA_INITIALIZATION_TYPE',
+    'AWS_LAMBDA_LOG_GROUP_NAME',
+    'AWS_LAMBDA_LOG_STREAM_NAME',
+    'AWS_LAMBDA_RUNTIME_API',
+    'AWS_REGION',
+    'AWS_SECRET_ACCESS_KEY',
+    'AWS_SESSION_TOKEN',
+    'AWS_XRAY_CONTEXT_MISSING',
+    'AWS_XRAY_DAEMON_ADDRESS',
+    'LAMBDA_RUNTIME_DIR',
+    'LAMBDA_TASK_ROOT',
+    'LANG',
+    'LD_LIBRARY_PATH',
+    'PATH',
+    'PWD',
+    'PYTHONPATH',
+    'SHLVL',
+    'TZ',
+    '_AWS_XRAY_DAEMON_ADDRESS',
+    '_AWS_XRAY_DAEMON_PORT',
+    '_HANDLER',
+    '_X_AMZN_TRACE_ID',
+    # These seems to be present when warmstarting, but no need to wipe them I guess?
+    'AWS_LAMBDA_FUNCTION_VERSION',
+    'AWS_SESSION_TOKEN',
 ]
 
 
 def main(lambda_event, _):
-    for key in CUSTOMER_ENVIRONMENT_VARIABLES:
-        os.environ.pop(key, None)
+    print('Environment variables start')
+    for key in list(os.environ):
+        if key not in COMMON_ENV:
+            os.environ.pop(key)
+            print(f'Remove {key} from env')
 
     for environment_variable in lambda_event:
-        os.environ[environment_variable['name']] = environment_variable['value']
+        key, value = environment_variable['name'], environment_variable['value']
+        if key in COMMON_ENV:
+            print(f'Cannot set reserved variable {key}')
+        else:
+            os.environ[key] = value
+
     feedback_and_export()
